3b8f246949d1c4297093c7addab7778bb990346b
Niroshan Sugirtharatnam <codeniro@gmail.com>
2015-04-18 14:06:54 -0400

    added functional test: run PHPUnit with PUW arguments

---
+++ functional/puw.test.js
@@ -0,0 +1,22 @@
+
+describe('PUW', function() {
+
+  it('should pass all PHPUnit command-line arguments from PUW to PHPUnit', function(done) {
+    var output = '',
+      exec = require('child_process').exec;
+
+    var child = exec('node puw fixtures/FailingTest.php');
+
+    child.stdout.on('data', function(data) {
+      output += data;
+    });
+
+    child.on('close', function() {
+      expect(output).to.match(/FAILURES!/);
+      done();
+    });
+
+  });
+
+
+});
